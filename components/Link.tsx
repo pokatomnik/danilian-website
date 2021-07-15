fc4387650df3822016fe13e417ad84100a229a60
Danilian Akhmedzianov <hugefast@gmail.com>
2021-07-16 01:51:17 +0300

    fix typings

--- components/Link.tsx
+++ components/Link.tsx
@@ -1,6 +1,13 @@
 import React from 'react';
 import styles from './Link.module.css';
 
-export function Link(props: React.PropsWithChildren<object>) {
-  return <a className={styles.a}>{props.children}</a>;
+interface ILinkProps {
+  rel?: string;
+  href?: string;
+  target?: string;
+  onClick?: (evt: React.MouseEvent<HTMLAnchorElement>) => void;
+}
+
+export function Link(props: React.PropsWithChildren<ILinkProps>) {
+  return <a className={styles.a} {...props} />;
 }
